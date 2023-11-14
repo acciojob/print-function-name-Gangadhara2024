951525980003bd23eee6cd20c0da71650b059420
Ruttala Gangadhara rao <136541746+Gangadhara2024@users.noreply.github.com>
2023-11-14 15:46:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 function functionName() {
-	alert("functionName");
-	 alert(arguments.callee.name);
+      alert(arguments.callee.name);
 }
 functionName();
 
 function AccioJob() {
-	alert("AccioJob");
 	 alert(arguments.callee.name);
 }
 AccioJob();
